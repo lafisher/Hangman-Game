3b91b681f0161f4fd3f6060a812b1ef061ccf6cc
Leigh Fisher <leighfisher@Jamess-MacBook-Air.local>
2017-02-07 23:30:21 -0600

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+//wowowowowowowowowowowow i have no idea what i'm doing. let's rock 
+  
+  var words =['wine','Lanaster','poison','lion','direwolf','swords','crows','feast',
+  'blood', 'dragon','Jamie','battle','death','wedding','Stark','heart tree',
+  ]
+console.log(words.length);
